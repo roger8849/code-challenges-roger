0fdf7480ef81e2e7d4f0933e4f21f908bc02e7c1
Roger Ramirez <roger8849@gmail.com>
2022-05-26 13:17:24 -0500

    Solve Leetcode 58 lenght of last word in python

--- python/leetcode/58_length_last_word.py
+++ python/leetcode/58_length_last_word.py
@@ -28,25 +28,58 @@ Constraints:
 s consists of only English letters and spaces ' '.
 There will be at least one word in s.
 '''
+# Most efficient. Start at end and then count the chars
 def lengthOfLastWord(s: str) -> int:
-    for letter in s:
-        lenght = 0
+    length_last_word = 0
+    i = len(s) - 1
+    while i >= 0:
+        letter = s[i]
         if letter != ' ':
-            lenght += 1
+            length_last_word +=1
+        elif letter == ' ' and length_last_word > 0 :
+            return length_last_word
+        i -= 1
+    return length_last_word
+
+
+# Reverse approach: reverse the string and count the chars.
+def lenghtOfLastWordReversed(s: str) -> int:
+    length_last_word = 0
+    s = s[::-1]
+    i=0
+    while(i < len(s)):
+        letter = s[i]
+        if letter != ' ':
+            length_last_word +=1
+        elif letter == ' ' and length_last_word > 0 :
+            return length_last_word
+        i += 1
+    return length_last_word
+
+
+# Naive approach start from the beggining and check all the letters.
+def lengthOfLastWordNaive(s: str) -> int:
+    length_last_word = 0
+
+    iterator = iter(s)
+    for letter in iterator:
+        if letter != ' ':
+            length_last_word += 1
         else:
-            if letter == ' ':
-                continue
-            if next(s) == None:
-                return letter
-            else:
-                letter =0
-    return lenght
-
-
+            while letter == ' ':
+                # Avoid problem when there is no next and the iterator is over.
+                letter = next(iterator, None)
+            else: 
+                if letter == None:
+                    return length_last_word
+                else:
+                    length_last_word=1
+    return length_last_word
 
 def main():
     phrase = input('Enter a word please: ')
-    print(lengthOfLastWord(phrase))
+    print(lengthOfLastWordNaive(phrase))
+    print(lenghtOfLastWordReversed(phrase))
 
 if __name__ == "__main__":
     main()
