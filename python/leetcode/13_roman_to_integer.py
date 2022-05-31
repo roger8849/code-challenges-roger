2033e008667dd48e296a4a596c409afe9d02bd6e
Roger Ramirez <roger8849@gmail.com>
2022-05-30 22:40:13 -0500

    Solve 2 leetcode roman to integer.

---
+++ python/leetcode/13_roman_to_integer.py
@@ -0,0 +1,80 @@
+'''
+13. Roman to Integer
+Easy
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+ 
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = ""
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+'''
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = int()
+        roman_symbols = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        size = len(s)
+        for i in range(size):
+            val_1 = roman_symbols[s[i]]
+            # If there is no next element then it's value is 0.
+            val_2 = 0 if i == size - 1 else roman_symbols[s[i+1]]
+            # If the next value is larger then val_1 is negative.
+            if val_1 < val_2:
+                val_1 *=-1
+            sum += val_1 
+        return sum
+
+def main():
+    solution = Solution()
+    print(solution.romanToInt('III'))
+    print(solution.romanToInt('LVIII'))
+    print(solution.romanToInt('MCMXCIV'))
+
+if __name__ == "__main__":
+    main()
