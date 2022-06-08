fcb8dda079ccdd03f9115b8650617fa30d427e87
Roger Ramirez <roger8849@gmail.com>
2022-06-08 16:38:24 -0500

    Removin unnecesary index variable.

--- python/algorithms/recursion/array_is_sorted_recursive.py
+++ python/algorithms/recursion/array_is_sorted_recursive.py
@@ -2,14 +2,14 @@
     Check if an array is sorted using recursion
 '''
 
-def array_is_sorted(a, start=0) -> bool:
+def array_is_sorted(a) -> bool:
     # Base case
     if len(a) <= 1 :
         return True
     # Induction hiphotesis sub problem is also sorted
-    small_answer = array_is_sorted(a[start + 1:])
+    small_answer = array_is_sorted(a[1:])
     # Induction step
-    return small_answer and a[start] <= a[start + 1]
+    return small_answer and a[0] <= a[1]
 
 
 def main():
@@ -17,6 +17,8 @@ def main():
     print(f'Array a = {a} is sorted {array_is_sorted(a)}')
     b = [1,2,6,4,5,6]
     print(f'Array b = {b} is sorted {array_is_sorted(b)}')
+    a = [-1,-10]
+    print(f'Array a = {a} is sorted {array_is_sorted(a)}')
 
 if __name__ == "__main__":
     main()
