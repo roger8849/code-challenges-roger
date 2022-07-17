e7a0b15511c6eb2474b5a430951252d26808d8a8
Roger Ramirez <roger8849@gmail.com>
2022-07-16 20:31:52 -0500

    Add binary tree order traversal

--- python/leetcode/103_Binary Tree Zigzag Level Order Traversal.py
+++ python/leetcode/103_Binary Tree Zigzag Level Order Traversal.py
@@ -1,34 +1,27 @@
 '''
-102. Binary Tree Level Order Traversal
+103. Binary Tree Zigzag Level Order Traversal
 Medium
 
-Share
-Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
-
- 
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
 
 Example 1:
-
-
 Input: root = [3,9,20,null,null,15,7]
-Output: [[3],[9,20],[15,7]]
-Example 2:
+Output: [[3],[20,9],[15,7]]
 
+Example 2:
 Input: root = [1]
 Output: [[1]]
-Example 3:
 
+Example 3:
 Input: root = []
 Output: []
- 
 
 Constraints:
 
 The number of nodes in the tree is in the range [0, 2000].
--1000 <= Node.val <= 1000
+-100 <= Node.val <= 100
 '''
 
-# Definition for a binary tree node.
 from typing import List, Optional
 
 
@@ -38,11 +31,11 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         result, levelArray, helperQueue = [], [], []
+        leftToRight = True
         if not root:
             return result
-
         helperQueue.append(root)
         helperQueue.append(None)
         while helperQueue:
@@ -58,15 +51,20 @@ class Solution:
                 if firstElement.right:
                     helperQueue.append(firstElement.right)
             else:
+                if not leftToRight:
+                    levelArray = levelArray[::-1]
                 # Push existing and then start a new level.
                 if levelArray:
                     result.append(levelArray)
                 # Start a new level.
                 levelArray=[]
+                # Once a level is completed then revert the order.
+                leftToRight = not leftToRight
                 # If queue still has elements then add flag for new level.
                 if helperQueue:
                     helperQueue.append(None)
         return result
+
 def main():
     sol = Solution()
     root = TreeNode(1)
@@ -76,8 +74,8 @@ def main():
     root.right = TreeNode(3)
     root.right.right = TreeNode(5)
 
-    result = sol.levelOrder(root)
-    assert result == [[1], [2,3], [4,5]]
+    result = sol.zigzagLevelOrder(root)
+    assert result == [[1], [3,2], [4,5]]
 
 
 if __name__ == "__main__":
---
+++ python/leetcode/107_Binary Tree Level Order Traversal II.py
@@ -0,0 +1,76 @@
+'''
+107. Binary Tree Level Order Traversal II
+Medium
+
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+'''
+# Definition for a binary tree node.
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result, levelArray, helperStack = [], [], []
+        if not root:
+             return result
+        helperStack.insert(0, root)
+        helperStack.insert(0, None)
+
+        while helperStack:
+            topElement = helperStack.pop()
+            if topElement:
+                levelArray.append(topElement.val)
+                if topElement.left:
+                    helperStack.insert(0, topElement.left)
+                if topElement.right:
+                    helperStack.insert(0, topElement.right)
+            else:
+                if levelArray:
+                    result.insert(0, levelArray)
+
+                levelArray = []
+
+                if helperStack:
+                    helperStack.insert(0, None)
+        return result
+
+def main():
+    sol = Solution()
+
+    root = TreeNode(1)
+
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(4)
+
+    root.right = TreeNode(3)
+    root.right.right = TreeNode(5)
+
+    result = sol.levelOrderBottom(root)
+    assert result == [[4,5], [2,3], [1]]
+
+
+if __name__ == "__main__":
+    main()
+
+
