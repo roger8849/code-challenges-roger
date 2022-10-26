55e16df2186652735d8deeedee388d0608644d9a
Roger Ramirez <roger8849@gmail.com>
2022-10-25 22:50:15 -0500

    Solve Missing numbers.

---
+++ python/hackerrank/Missing Numbers.py
@@ -0,0 +1,45 @@
+'''
+Missing Numbers
+https://www.hackerrank.com/challenges/missing-numbers/problem?isFullScreen=true
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'missingNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY brr
+#
+
+def missingNumbers(arr, brr):
+    # Write your code here
+    first, second = Counter(brr), Counter(arr)
+    return sorted(list((first - second).keys()))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
