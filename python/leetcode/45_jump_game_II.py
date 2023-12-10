6bc93ea6b99c317957babb1957c3765ed17eeb19
roger8849 <3719300+roger8849@users.noreply.github.com>
2023-12-10 17:44:54 +0000

    Add replit files.
    Add replit project files.
    Add further explanation of jump_game_II algortithm.

--- python/leetcode/45_jump_game_II.py
+++ python/leetcode/45_jump_game_II.py
@@ -31,34 +31,39 @@ from typing import List
 
 
 class Solution:
-    def jump(self, nums: List[int]) -> int:
-        size = len(nums)
-        if size == 1: return 0
-        if nums[0] == 0: return -1
-        
-        ladder = nums[0] # Max reachable index
-        stair = nums [0] # current ladder
-        jump = 1
-        for i in range(1, size):
-            if i == size-1: return jump
-            if i + nums[i] > ladder:
-                ladder = i + nums[i]
-            stair -= 1 # Traversing current ladder
 
-            if stair == 0: # current ladder finished
-                jump += 1
-                if i >= ladder: return -1 # Cannot reach any index
-                stair = ladder - i
+  def jump(self, nums: List[int]) -> int:
+    size = len(nums)
+    if size == 1: return 0
+    if nums[0] == 0: return -1
 
-        return -1
+    ladder = nums[0]  # Max reachable index
+    stair = nums[0]  # current ladder
+    jump = 1
+    for i in range(1, size):
+      if i == size - 1: return jump
+      if i + nums[i] > ladder:
+        ladder = i + nums[i]
+      stair -= 1  # Traversing current ladder
+
+      if stair == 0:  # current ladder finished
+        jump += 1
+        if i >= ladder:
+          return -1  # Additionally, we have a check for whether the current index "i" has exceeded the maximum reachable index "ladder". If this is the case, it means that we cannot reach any index further, so -1 is returned to indicate this. The "stair" variable is then updated to the remaining distance to the "ladder."
+        stair = ladder - i
+
+    return -1
 
 
 def main():
-    solution = Solution()
-    nums = [2,3,1,1,4]
-    print(f'Number of steps to reach last index in {nums} = {solution.jump(nums)}')
-    nums = [3,2,1,0,4]
-    print(f'Number of steps to reach last index in {nums} = {solution.jump(nums)}')
+  solution = Solution()
+  nums = [2, 3, 1, 1, 4]
+  print(
+      f'Number of steps to reach last index in {nums} = {solution.jump(nums)}')
+  nums = [3, 2, 1, 0, 4]
+  print(
+      f'Number of steps to reach last index in {nums} = {solution.jump(nums)}')
+
 
 if __name__ == "__main__":
-    main()
+  main()
